ab9ce3cfe59b62cdd280610e28c9f3560ccaaa0f
Max <miskamgame@gmail.com>
2025-03-05 16:29:00 +0400

    1

--- src/test/java/back_test/soap_tests/ParamTest.java
+++ src/test/java/back_test/soap_tests/ParamTest.java
@@ -1,12 +1,14 @@
 package back_test.soap_tests;
 
 import back_test.base_tests.BaseTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ParamTest extends BaseTest {
     String es_host = System.getProperty("ES_HOST");
+    @DisplayName("WowTest")
     @Test
     public void getParamFromJenkinsTest(){
         assertThat("4").isEqualTo("4");
